65a3a415c67f66d3041f6b8db72ac9ef047e4798
Baylsan-Ml <bailsanmletat99@gmail.com>
2025-11-19 17:24:25 +0200

    SPA

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import Navbar from './components/navbar/Navbar.jsx'
+import Footer from './components/footer/Footer.jsx'
+import Home from './pages/home/Home.jsx'
+import User from './pages/user/User.jsx'
+import Register from './pages/register/Register.jsx'
+import Users from './pages/user/Users.jsx'
+import AddUser from './pages/user/AddUser.jsx'
+import { Route, Routes } from 'react-router-dom'
+
 
 export default function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='Users' element={<Users />} />
+      <Route path='Users/User' element={<User />} />
+      <Route path='Users/AddUser' element={<AddUser />} />
+      <Route path='Register' element={<Register />} />
+    </Routes>
+    <Footer /> 
+    </>
   )
 }
